9fae3a73ccb327b76dd90c57427cbf778c778ef7
FreezingFruit <arenzmauricio12@gmail.com>
2025-01-15 21:13:33 +0800

    Login

---
+++ knowledge-nuggets/src/Login.jsx
@@ -0,0 +1,47 @@
+import { useRef, useState, useEffect, useContext } from "react";
+
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  return (
+    <section>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          value={pwd}
+          required
+        />
+        <button>Sign In</button>
+      </form>
+      <p>
+        Need an Account?
+        <br />
+        <span className="line">
+          {/*put router link here*/}
+          <a href="#">Sign Up</a>
+        </span>
+      </p>
+    </section>
+  );
+};
+export default Login;
