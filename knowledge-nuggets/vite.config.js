dbbbc092aef219f5825cd84a2f22c2cad18a6b5e
merphiel <agomaa.robet@gmail.com>
2025-03-01 19:33:15 +0800

    yessir

--- knowledge-nuggets/vite.config.js
+++ knowledge-nuggets/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0', // Expose to all network interfaces
-    port: 3000 // Optional: specify port
+     base: '/Knowledge-Nuggets/'
   },
   plugins: [react()],
 })
