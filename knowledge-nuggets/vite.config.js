3ee381f8b7be9bcd8a614ba571ca84af81a7d371
merphiel <agomaa.robet@gmail.com>
2025-03-01 19:41:01 +0800

    a

--- knowledge-nuggets/vite.config.js
+++ knowledge-nuggets/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Knowledge-Nuggets/',
   server: {
-     base: '/Knowledge-Nuggets/'
+    // server configuration if needed
   },
   plugins: [react()],
 })
