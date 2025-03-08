3c3e7b9774cdff13c8c67291e7e655c2b4442d31
merphiel <agomaa.robet@gmail.com>
2025-03-08 15:40:13 +0800

    Update API base URL in Home component

--- knowledge-nuggets/Home.jsx
+++ knowledge-nuggets/Home.jsx
@@ -11,7 +11,7 @@ import { jsPDF } from "jspdf";
 
 // Define a consistent API base URL
 const API_BASE_URL =
-  "https://papua-rap-treating-glucose.trycloudflare.com"; // Use localhost or your server IP
+  "https://clicks-filtering-david-abc.trycloudflare.com"; // Use localhost or your server IP
 
 const Home = () => {
   const [url, setUrl] = useState("");
